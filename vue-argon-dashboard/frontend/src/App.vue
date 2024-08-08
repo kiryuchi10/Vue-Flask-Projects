dbc932f080624fbdcd3c4cc481c72959988030b6
Kiryuchi <141597057+kiryuchi10@users.noreply.github.com>
2024-08-08 09:45:38 +0900

    240808 commit
    vuetify used

--- vue-argon-dashboard/frontend/src/App.vue
+++ vue-argon-dashboard/frontend/src/App.vue
@@ -1,42 +1,65 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/login">Login</router-link>
-      <router-link to="/signup">Sign Up</router-link>
-    </nav>
-    <router-view></router-view>
-  </div>
+  <v-card
+    class="mx-auto"
+    max-width="500"
+  >
+    <v-card-item class="bg-cyan-darken-1">
+      <v-card-title>
+        <span class="text-h5">Sarah Mcbeal</span>
+      </v-card-title>
+
+      <template v-slot:append>
+        <v-defaults-provider
+          :defaults="{
+            VBtn: {
+              variant: 'text',
+              density: 'comfortable',
+            }
+          }"
+        >
+          <v-btn icon="mdi-chevron-left"></v-btn>
+
+          <v-btn icon="mdi-pencil"></v-btn>
+
+          <v-btn icon="mdi-dots-vertical"></v-btn>
+        </v-defaults-provider>
+      </template>
+    </v-card-item>
+
+    <v-list>
+      <v-list-item
+        append-icon="mdi-message-text"
+        prepend-icon="mdi-phone"
+        title="(650) 555-1234"
+      ></v-list-item>
+
+      <v-divider inset></v-divider>
+
+      <v-list-item
+        append-icon="mdi-message-text"
+        prepend-icon="mdi-phone"
+        title="(323) 555-6789"
+      ></v-list-item>
+
+      <v-divider inset></v-divider>
+
+      <v-list-item
+        prepend-icon="mdi-email"
+        title="mcbeal@example.com"
+      ></v-list-item>
+
+      <v-divider inset></v-divider>
+
+      <v-list-item
+        prepend-icon="mdi-map-marker"
+        title="Orlando, FL 79938"
+      ></v-list-item>
+    </v-list>
+
+    <v-img
+      height="200"
+      src="https://picsum.photos/700?image=996"
+      cover
+    ></v-img>
+  </v-card>
 </template>
-
-<script>
-export default {
-  name: 'App',
-};
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-nav {
-  display: flex;
-  justify-content: center;
-  margin-bottom: 20px;
-}
-
-nav a {
-  margin: 0 15px;
-  text-decoration: none;
-  color: #42b983;
-}
-
-nav a.router-link-exact-active {
-  font-weight: bold;
-}
-</style>
--- vue-argon-dashboard/frontend/vue.config.js
+++ vue-argon-dashboard/frontend/vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   devServer: {
     proxy: 'http://localhost:5000'
+  },
+
+  pluginOptions: {
+    vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
   }
 };
